340031e353b67bfa89b2a85d4828b5ec053606f5
kristian woods <kris_woods@hotmail.co.uk>
2019-06-08 10:22:15 +0100

    first commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# mobile nav
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/Woodenchops/pen/oRPRjO](https://codepen.io/Woodenchops/pen/oRPRjO).
+
+ 
---
+++ dist/script.js
@@ -0,0 +1,48 @@
+function MobileNav(props) {
+    this._nav = props.nav;
+    this._parentElement = props.parentElement;
+    this._trigger = props.trigger;
+    this._activeClass = props.activeClass;
+    this._props = props;
+
+    var self = this;
+    var parentElement = this._parentElement;
+    var nav = parentElement.querySelector(this._nav);
+    var trigger = parentElement.querySelector(this._trigger);
+    
+    this.setState = function(state) {
+        (typeof state === 'boolean') ? this._state = state : this._state = false;
+        (this._state) ? nav.classList.add(this._activeClass) : nav.classList.remove(this._activeClass);
+    }
+
+    this.getState = function() {
+        return this._state;
+    }
+
+    this.toggleState = function() {
+        this.setState(!this.getState());
+    }
+  
+  trigger.addEventListener('click', function(e) {
+    e.preventDefault();
+    this.toggleState();
+  }.bind(self));
+
+    
+    
+}
+
+var navs = document.querySelectorAll('.wrap')
+
+var buttonsArray = [];
+
+
+navs.forEach(function(nav) {
+  buttonsArray.push(
+      new MobileNav({
+      parentElement: nav,
+      nav: '#nav',
+      trigger: '#nav-btn',
+      activeClass: 'active'
+  }));
+});
