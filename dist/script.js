fa806c8dab7060b79fc352b6c0e1b50129c09afa
kristian woods <kris_woods@hotmail.co.uk>
2019-06-08 13:20:05 +0100

    update

--- dist/script.js
+++ dist/script.js
@@ -1,48 +1,64 @@
 function MobileNav(props) {
-    this._nav = props.nav;
-    this._parentElement = props.parentElement;
-    this._trigger = props.trigger;
-    this._activeClass = props.activeClass;
-    this._props = props;
+  this._parentElement = props.parentElement;
+  this._nav = this._parentElement.querySelector(props.nav);
+  this._trigger = this._parentElement.querySelector(props.trigger);
+  this._activeClass = props.activeClass;
+  this._props = props;
+  this._open = props.open;
+  this._self = this;
 
-    var self = this;
-    var parentElement = this._parentElement;
-    var nav = parentElement.querySelector(this._nav);
-    var trigger = parentElement.querySelector(this._trigger);
-    
-    this.setState = function(state) {
-        (typeof state === 'boolean') ? this._state = state : this._state = false;
-        (this._state) ? nav.classList.add(this._activeClass) : nav.classList.remove(this._activeClass);
-    }
+  this.openOrClose();
 
-    this.getState = function() {
-        return this._state;
-    }
-
-    this.toggleState = function() {
-        this.setState(!this.getState());
-    }
-  
-  trigger.addEventListener('click', function(e) {
+  this._trigger.addEventListener('click', function (e) {
     e.preventDefault();
     this.toggleState();
-  }.bind(self));
-
-    
-    
+  }.bind(this._self));
 }
 
-var navs = document.querySelectorAll('.wrap')
+MobileNav.prototype = function () {
+
+
+  var setState = function (state) {
+    (typeof state === 'boolean') ? this._state = state : this._state = false;
+    (this._state) ? this._nav.classList.add(this._activeClass): this._nav.classList.remove(this._activeClass);
+  }
+
+  var getState = function () {
+    return this._state;
+  }
+
+  var toggleState = function () {
+    this.setState(!this.getState());
+  }
+
+  var openOrClose = function() {
+    if(this._open) {
+      this.setState(true);
+    } else {
+      this.setState(false);
+    }
+  }
+
+  return {
+    setState: setState,
+    getState: getState,
+    toggleState: toggleState,
+    openOrClose: openOrClose
+  }
+
+}()
+
+var navs = document.querySelectorAll('.wrap');
 
 var buttonsArray = [];
 
-
-navs.forEach(function(nav) {
-  buttonsArray.push(
-      new MobileNav({
-      parentElement: nav,
-      nav: '#nav',
-      trigger: '#nav-btn',
-      activeClass: 'active'
+navs.forEach(function (nav) {
+  buttonsArray.push(new MobileNav({
+    parentElement: nav,
+    nav: '#nav',
+    trigger: '#nav-btn',
+    activeClass: 'active',
+    open: false
   }));
 });
+
